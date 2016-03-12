49029e26409679102320bed329567444e146ca51
Samuel San <samuelsaninbox@gmail.com>
2016-03-12 16:17:04 -0500

    Updated user branch from Portal workspace (server script)

--- serverscript.js
+++ serverscript.js
@@ -18,7 +18,7 @@ function createTable() {
     var row = JSON.parse(queryResult);
 
     if (row.length > 0 && typeof row[0].Error != 'undefined') {
-        db.Execute('CREATE TABLE sampleTable(id INTEGER PRIMARY KEY IDENTITY(1,1), userId nvarchar(50), value nvarchar(50), description nvarchar(500), contact nvarchar(200), forSale integer, tutoring integer, housing integer, lostFound integer, status integer);'); 
+        db.Execute('CREATE TABLE sampleTable(id INTEGER PRIMARY KEY IDENTITY(1,1), userId nvarchar(50), value nvarchar(50), description nvarchar(500), contact nvarchar(200), category integer, status integer);'); 
         //status 1 = buy, 0 = sell;
         result = '{"status":"tableCreated"}';
     } else
@@ -29,6 +29,6 @@ function createTable() {
 
 // Insert into the database
 function insert() {
-    db.Execute('INSERT INTO sampleTable (userId, value, description, contact, forSale, tutoring, housing, lostFound, status) VALUES(@currentUser,@value,@description,@contact,@forSale,@tutoring,@housing,@lostFound,@status)');
+    db.Execute('INSERT INTO sampleTable (userId, value, description, contact, category, status) VALUES(@currentUser,@value,@description,@contact,@category,@status)');
     return getData();     
 } 
