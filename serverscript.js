71d7fed805d416ee930f55803896cd3912e45862
Samuel San <samuelsaninbox@gmail.com>
2016-03-12 15:02:32 -0500

    Updated user branch from Portal workspace (server script)

--- serverscript.js
+++ serverscript.js
@@ -30,16 +30,7 @@ function createTable() {
 function insert() {
     console.log(args.Get("value"));
     console.log(args.Get("description"));
-    if (args.Get("value").length > 50)
-        return '{"result":"error"}';
-    else {
-        db.Execute('INSERT INTO sampleTable (userId, value) VALUES(@currentUser,@value)');
-        return getData();
-    }    
-    if (args.Get("description").length > 500)
-        return '{"result":"error"}';
-    else {
-        db.Execute('INSERT INTO sampleTable (userId, description) VALUES(@currentUser,@description)');
+        db.Execute('INSERT INTO sampleTable (userId, value, description) VALUES(@currentUser,@value,@description)');
         return getData();
     }       
 } 
