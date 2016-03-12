a4249a19e11f14402644372e74ec82165a333044
Samuel San <samuelsaninbox@gmail.com>
2016-03-12 15:10:39 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -3,186 +3,189 @@ angular.module('portalApp')
 // Widget controller - runs every time widget is shown
 .controller('watIsOurTeamNameCtrl', ['$scope', '$http', '$q', 'watIsOurTeamNameFactory', function($scope, $http, $q, watIsOurTeamNameFactory) {
 
-        // Widget Configuration
-        $scope.portalHelpers.config = {
-            // make 'widgetMenu.html' the template for the top right menu
-            "widgetMenu": "widgetMenu.html"
-        };
+            // Widget Configuration
+            $scope.portalHelpers.config = {
+                // make 'widgetMenu.html' the template for the top right menu
+                "widgetMenu": "widgetMenu.html"
+            };
 
-        // Import variables and functions from service
-        $scope.loading = watIsOurTeamNameFactory.loading;
-    	$scope.insertValue = watIsOurTeamNameFactory.insertValue;
-        $scope.insertDescription = watIsOurTeamNameFactory.insertDescription;
-    	$scope.item = {value:''};
-    	$scope.dbData = watIsOurTeamNameFactory.dbData;
-        // $scope.data = watIsOurTeamNameFactory.data;
-        // Model for the search and list example
-        $scope.model = [{
-            title: "item 1",
-            details: "item 1 details",
-            category: '1'
-        }, {
-            title: "item 2",
-            details: "item 2 details",
-            category: '2'
-        }, {
-            title: "item 3",
-            details: "item 3 details",
-            category: '1'
-        }, {
-            title: "item 4",
-            details: "item 4 details",
-            category: '2'
-        }, {
-            title: "item 5",
-            details: "item 5 details",
-            category: '1'
-        }, {
-            title: "item 6",
-            details: "item 6 details",
-            category: '2'
-        }];
+            // Import variables and functions from service
+            $scope.loading = watIsOurTeamNameFactory.loading;
+            $scope.insertValue = watIsOurTeamNameFactory.insertValue;
+            $scope.insertDescription = watIsOurTeamNameFactory.insertDescription;
+            $scope.item = {
+                value: ''
+            };
+            $scope.dbData = watIsOurTeamNameFactory.dbData;
+            // $scope.data = watIsOurTeamNameFactory.data;
+            // Model for the search and list example
+            $scope.model = [{
+                title: "item 1",
+                details: "item 1 details",
+                category: '1'
+            }, {
+                title: "item 2",
+                details: "item 2 details",
+                category: '2'
+            }, {
+                title: "item 3",
+                details: "item 3 details",
+                category: '1'
+            }, {
+                title: "item 4",
+                details: "item 4 details",
+                category: '2'
+            }, {
+                title: "item 5",
+                details: "item 5 details",
+                category: '1'
+            }, {
+                title: "item 6",
+                details: "item 6 details",
+                category: '2'
+            }];
 
-        // initialize the service
-        watIsOurTeamNameFactory.init($scope);
+            // initialize the service
+            watIsOurTeamNameFactory.init($scope);
 
-        // Show main view in the first column
-        $scope.portalHelpers.showView('main.html', 1);
+            // Show main view in the first column
+            $scope.portalHelpers.showView('main.html', 1);
 
-        // watch for changes in the loading variable
-        $scope.$watch('loading.value', function() {
-            // if loading
-            if ($scope.loading.value) {
-                // show loading screen in the first column, and don't append it to browser history
-                $scope.portalHelpers.showView('loading.html', 1, false);
-                // show loading animation in place of menu button
-                $scope.portalHelpers.toggleLoading(true);
-            } else {
-                $scope.portalHelpers.showView('main.html', 1);
-                $scope.portalHelpers.toggleLoading(false);
+            // watch for changes in the loading variable
+            $scope.$watch('loading.value', function() {
+                // if loading
+                if ($scope.loading.value) {
+                    // show loading screen in the first column, and don't append it to browser history
+                    $scope.portalHelpers.showView('loading.html', 1, false);
+                    // show loading animation in place of menu button
+                    $scope.portalHelpers.toggleLoading(true);
+                } else {
+                    $scope.portalHelpers.showView('main.html', 1);
+                    $scope.portalHelpers.toggleLoading(false);
+                }
+            });
+
+            // Handle click on an item in the list and search example
+            $scope.showDetails = function(item) {
+                // Set which item to show in the details view
+                $scope.item.value = item;
+                // Show details view in the second column
+                $scope.portalHelpers.showView('details.html', 2);
+            };
+
+            // Handle "previous item" click from the details page
+            $scope.prevItem = function() {
+                // get previous items in the list
+                var prevItem = $scope.portalHelpers.getPrevListItem();
+                // refresh details view with the new item
+                $scope.showDetails(prevItem);
+            }
+
+            $scope.nextItem = function() {
+                    var nextItem = $scope.portalHelpers.getNextListItem();
+                    $scope.showDetails(nextItem);
+                }
+                // Post Ad stuff
+            $scope.showPostAd = function(item) {
+                // Set which item to show in the showAdDetails view
+                $scope.item.value = item;
+                $scope.item.description = item;
+                // Show details view in the second column
+                $scope.portalHelpers.showView('adDetails.html', 2);
+            };
+            //Create Table
+            $scope.createTable = function() {
+                    $scope.portalHelpers.invokeServerFunction('createTable').then(function(
+                        result) {
+                        $scope.dbData.value = [];
+                        $scope.dbData.description = [];
+                    });
+                }
+                // Handle form submit in the database test example
+            $scope.insertData = function() {
+                //title
+                $scope.portalHelpers.invokeServerFunction('insert', {
+                    value: $scope.insertValue.value,
+                    description: $scope.insertDescription.description
+                }).then(function(result) {
+                    $scope.dbData.value = result;
+                    $scope.dbData.description = result;
+                });
+            }
+            }])
+        // Factory maintains the state of the widget
+        .factory('watIsOurTeamNameFactory', ['$http', '$rootScope', '$filter', '$q', function($http, $rootScope, $filter, $q) {
+
+            var initialized = {
+                value: false
+            };
+            // Your variable declarations
+            var loading = {
+                value: true
+            };
+            var sourcesLoaded = 0;
+
+            // Your variable declarations
+            var data = {
+                value: null
+            };
+            var dbData = {
+                value: null
+            };
+            var insertValue = {
+                value: null
+            };
+            var insertDescription = {
+                description: null
+            };
+            var init = function($scope) {
+                if (initialized.value)
+                    return;
+
+                initialized.value = true;
+
+                // Place your init code here:
+                $scope.portalHelpers.invokeServerFunction('getData').then(function(result) {
+                    dbData.value = result;
+                    dbData.description = result;
+                });
+                data.value = {
+                    message: "Welcome to Waterloo's Classified Page"
+                };
+                sourceLoaded();
+            }
+
+            function sourceLoaded() {
+                sourcesLoaded++;
+                if (sourcesLoaded > 0)
+                    loading.value = false;
+            }
+
+
+            // Expose init(), and variables
+            return {
+                init: init,
+                data: data,
+                loading: loading,
+                insertValue: insertValue,
+                insertDescription: insertDescription,
+                dbData: dbData
+            };
+
+        }])
+        // Custom directive example
+        .directive('watIsOurTeamNameDirectiveName', ['$http', function($http) {
+            return {
+                link: function(scope, el, attrs) {
+
+                }
+            };
+        }])
+        // Custom filter example
+        .filter('watIsOurTeamNameFilterName', function() {
+            return function(input, arg1, arg2) {
+                // Filter your output here by iterating over input elements
+                var output = input;
+                return output;
             }
         });
-
-        // Handle click on an item in the list and search example
-        $scope.showDetails = function(item) {
-            // Set which item to show in the details view
-            $scope.item.value = item;
-            // Show details view in the second column
-            $scope.portalHelpers.showView('details.html', 2);
-        };
-
-        // Handle "previous item" click from the details page
-        $scope.prevItem = function() {
-            // get previous items in the list
-            var prevItem = $scope.portalHelpers.getPrevListItem();
-            // refresh details view with the new item
-            $scope.showDetails(prevItem);
-        }
-
-        $scope.nextItem = function() {
-            var nextItem = $scope.portalHelpers.getNextListItem();
-            $scope.showDetails(nextItem);
-        }
-        // Post Ad stuff
-        $scope.showPostAd = function(item) {
-            // Set which item to show in the showAdDetails view
-            $scope.item.value = item;
-            $scope.item.description = item;
-            // Show details view in the second column
-            $scope.portalHelpers.showView('adDetails.html', 2);
-        };
-        //Create Table
-        $scope.createTable = function () {
-            $scope.portalHelpers.invokeServerFunction('createTable').then(function (
-                result) {
-                $scope.dbData.value = [];
-                $scope.dbData.description = [];                
-            });
-        }
-        // Handle form submit in the database test example
-        $scope.insertData = function () {
-            //title
-            $scope.portalHelpers.invokeServerFunction('insert', {
-                value: $scope.insertValue.value, 
-                description: $scope.insertDescription.description
-            }).then(function (result) {
-                $scope.dbData.value = result;
-                $scope.dbData.description = result;
-            });
-    	}])
-    // Factory maintains the state of the widget
-    .factory('watIsOurTeamNameFactory', ['$http', '$rootScope', '$filter', '$q', function($http, $rootScope, $filter, $q) {
-
-        var initialized = {
-            value: false
-        };
-        // Your variable declarations
-        var loading = {
-            value: true
-        };
-        var sourcesLoaded = 0;
-
-        // Your variable declarations
-        var data = {
-            value: null
-        };
-        var dbData = {
-            value: null
-        };
-        var insertValue = {
-            value: null
-        };      
-        var insertDescription = {
-            description: null
-        };       
-        var init = function($scope) {
-            if (initialized.value)
-                return;
-
-            initialized.value = true;
-
-            // Place your init code here:
-            $scope.portalHelpers.invokeServerFunction('getData').then(function (result) {
-                dbData.value = result;
-                dbData.description = result;
-            });
-            data.value = {
-                message: "Welcome to Waterloo's Classified Page"
-            };
-            sourceLoaded();
-        }
-
-        function sourceLoaded() {
-            sourcesLoaded++;
-            if (sourcesLoaded > 0)
-                loading.value = false;
-        }
-
-
-        // Expose init(), and variables
-        return {
-            init: init,
-            data: data,
-            loading: loading,
-            insertValue: insertValue,
-            insertDescription: insertDescription,
-            dbData: dbData            
-        };
-
-    }])
-    // Custom directive example
-    .directive('watIsOurTeamNameDirectiveName', ['$http', function($http) {
-        return {
-            link: function(scope, el, attrs) {
-
-            }
-        };
-    }])
-    // Custom filter example
-    .filter('watIsOurTeamNameFilterName', function() {
-        return function(input, arg1, arg2) {
-            // Filter your output here by iterating over input elements
-            var output = input;
-            return output;
-        }
-    });
